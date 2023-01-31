8f7143ae7931f567e716111d376b0f7a13c6e14c
jacquelynneHeiman <jacquelynne.heiman@gmail.com>
2023-01-30 22:37:45 -0800

    Initial Commit

---
+++ LabBench/Pages/SampleLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LabBench.Pages
+{
+    public class SampleLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LabBench/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function toggleNav() {
+    if (document.getElementById("sideNav").style.width == "250px") {
+        closeNav();
+    }
+    else {
+        openNav();
+    }
+}
+
+function openNav() {
+    document.getElementById("sideNav").style.width = "260px";
+    document.getElementById("topNavLogo").style.zIndex = "2";
+    document.getElementById("closeBtn").style.display = "block";
+
+    document.getElementById("mainContainer").style.width = (window.innerWidth - 260) + "px";
+    document.getElementById("mainContainer").style.marginLeft = "260px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+    document.getElementById("topNavLogo").style.marginLeft = "0";
+
+    document.getElementById("mainContainer").style.width = "100%";
+    document.getElementById("mainContainer").style.marginLeft = "0";
+
+    document.getElementById("closeBtn").style.display = "none";
+}
+
+
